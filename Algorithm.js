25ea2373d27bf5df83402c2bda4f4542ba83e1f2
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2018-11-28 18:33:26 +0900

    ほぼ全面的にプログラムを変更しました． 打点→最近傍法を使ってリンクドリストに格納→線の描画→距離算出まで． この後は2-opt改善法による入れ替えを実装予定．

--- Algorithm.js
+++ Algorithm.js
@@ -2,104 +2,162 @@ class Point {
     constructor(x, y) {
         this.x = x;       //x座標
         this.y = y;　　　　//y座標
+        this.next = null;   // 次の点
+        this.temp = null;   // 暫定的な次の点
     }
 
-    distance(p)//長さを求める
+    /**
+     * この点からパラメータで与えられた点までの距離を計算する
+     * 呼び出すときはp1.distance(p2)のように書く
+     * @param p {Point} - 距離を計算したい点
+     * @returns {number} - 距離
+     */
+    distance( p )//長さを求める
     {
         return (Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2)));//三平方の定理で距離を求める
-    }//呼び出すときはp1.distance(p2)のように書く
+    }
 
-}//円をクラス化
+    /**
+     * この点から次の点までの距離を計算する
+     * @returns {number} - 距離
+     */
+    distance_chain()
+    {
+        let start = this;
+        var pi = start;
+        let distance = 0;
+        do {
+            distance += Math.sqrt(Math.pow(this.x - this.next.x, 2) + Math.pow(this.y - this.next.y, 2))
+            pi = pi.next;
+        } while(pi != start);
+
+        return distance;
+    }
+
+    /**
+     * この点から，暫定的な次の点までの距離を計算する
+     * @returns {number} - 距離
+     */
+    distance_temp()
+    {
+        let start = this;
+        var pi = start;
+        let distance = 0;
+        do {
+            if (this.temp != null) {
+                distance += Math.sqrt(Math.pow(this.x - this.temp.x, 2) + Math.pow(this.y - this.temp.y, 2))
+                pi = pi.temp;
+            } else {
+                distance += Math.sqrt(Math.pow(this.x - this.next.x, 2) + Math.pow(this.y - this.next.y, 2))
+                pi = pi.next;
+            }
+        } while(pi != start);
+
+        return distance;
+    }
+
+    chain( p ) {
+        this.next = p;
+    }
+
+    change( p ) {
+        this.temp = p.next;
+        p.temp = this.next;
+    }
+
+}
+
+
+
 
 class Graphics {
     constructor(number) {
         this.max = number;
         this.route = [];
-        this.solve_canvas = document.querySelector('#solve_canvas');
+        this.solve_canvas = document.querySelector('#solve_Canvas');
+        console.log( this.solve_canvas );
         this.ctx = this.solve_canvas.getContext('2d');
         this.pa = [];
         this.number = 0; //円の数
         this.solve_canvas.addEventListener('click', (ev) => {
             if (this.max > this.number) {　//指定した円の数より現在の円の数が下回る場合
                 let p = new Point(ev.offsetX, ev.offsetY);
+                console.log(p);
                 this.pa.push(p); //クリックして作った円を配列に入れ
                 this.number++;//円が書かれた個数
-                this.draw(ev.offsetX, ev.offsetY, 10);//サイズ10でクリックした位置に円を描く
+                this.draw_point(ev.offsetX, ev.offsetY, 10);//サイズ10でクリックした位置に円を描く
             }
         });//クリックされた場所を取得してdrawを呼び出す
 
-        document.getElementById("clear").addEventListener("click", () => {
+        document.querySelector("#clear").addEventListener("click", () => {
             this.ctx.clearRect(0, 0, 950, 420);
             this.pa = [];
             this.number = 0;
         });　　　　　　　　//リセットボタンを押した時に描画領域、配列、個数をリセット
 
-        document.getElementById("start").addEventListener('click', () => {//スタートボタンを押したとき最近傍法が描画される
-            let copied = [];
-            this.route = [];//経路を保存
-            let xorigin = 0;//始点のx座標
-            let yorigin = 0;//始点のy座標
-            for (let pp of this.pa) copied.push(pp);　//ppにpaを複製しcopiedにppの値をpushする
-            var pi = copied.shift();　//先頭を削除して詰めたものをpiに保存 ここでpiの値が変わる
-            if (xorigin == 0 && yorigin == 0) {
-                xorigin = pi.x;
-                yorigin = pi.y;
-            }
-            var xfst = pi.x;
-            var yfst = pi.y;//ここの代入の仕方
-            while (copied.length > 0) {
-                console.log(pi);
+        // 最近傍法によるルート設定
+        document.querySelector("#start").addEventListener('click', () => {
+            this.start = this.pa.shift();
+            var pi = this.start;
+            var count=0;
+            while (this.pa.length > 0) {
+                console.log(count++);
+                console.log(this.pa.length);
+                //console.log(pi);
                 let near = this.shortest(pi);　//nearにpiからの最短を保存
-                let next = copied.indexOf(near); //nextにnearを複製
-                pi = copied.splice(next, 1)[0]
-                this.route.push(pi);//routeにcopiedから削除した最短経路を入れる
-                var xshortest = pi.x; //終点のx座標
-                var yshortest = pi.y; //終点のy座標
-                this.line(xfst, yfst, xshortest, yshortest);//始点から最短に線を引く
-                xfst = xshortest;
-                yfst = yshortest;
+                let next = this.pa.indexOf(near); //nextにnearを複製
+                let p2 = this.pa.splice(next, 1)[0];
+                pi.chain( p2 );
+                console.log(pi);
+                //this.line(xfst, yfst, xshortest, yshortest);//始点から最短に線を引く
+                pi = p2;
             }
-            this.line(xshortest, yshortest, xorigin, yorigin);
-
-
-            console.log(this.route);
+            pi.chain( this.start );
+            this.draw_next();
         });
 
-        document.getElementById("start2").addEventListener('click', () => {
-            let pn = [];
-            for (let pn of this.route)//最近傍の経路をコピー//代入の仕方
-                for (let i = 0; i < this.max - 1; i++) {
-                    for (let j = 0; j < this.max - 1; j++) {//i,jそれぞれがthis.max-1の時について
-                        if (Math.abs(i - j) > 1) {//隣接していないならば//iかjがthis.max-1の時の処理
-                            extra1 = pn[i].distance(pn[j + 1]);//終点を入れ替える
-                            extra2 = pn[j].distance(pn[i + 1]);//終点を入れ替える
-                            if (pn[i].distance(pn[i + 1]) + pn[j].distance(pn[j + 1]) > extra1 + extra2) {// 元の長さと比べる
-                                xx = pn[i].x;
-                                yy = pn[i].y;
-                                xxx = pn[j].x;
-                                yyy = pn[j].y;
-                                xxxx = pn[i + 1].x;
-                                yyyy = pn[i + 1].y;
-                                xxxxx = pn[j + 1].x;
-                                yyyyy = pn[j + 1].y;
-                                this.cline(xx, yy, xxxx, yyyy);
-                                this.cline(xxx, yyy, xxxxx, yyyyy);//元あった線の色を変える
-                                this.tline(xx, yy, xxxxx, yyyyy);
-                                this.tline(xxx, yyy, xxxx, yyyy);//改善した経路を表示
-                            }
-                        }
-                    }
-                }
+        document.querySelector("#start2").addEventListener('click', () => {
+            var distance1 = this.start.distance_chain();
+            var distance2 = this.start.distance_temp();
+            console.log( distance1 );
+            console.log( distance2 );
+            // var pi = this.start;
+            // do {
+            //     console.log( "x=" + pi.x + ", y=" + pi.y );
+            //     pi = pi.next;
+            // } while(pi != this.start);
+
+            //this.draw_next();
+
+            // for( let i=0; i<this.max; i++ ) {
+            //     console.log( "---" );
+            //     console.log( pi );
+            //     console.log( pi.next );
+            //     if( pi.next != this.start ) console.log("続く");
+            //     else console.log("終わり");
+            //     pi = pi.next;
+            // }
         });
-        //都市の数this.maxをここで変更できるか
-        //this.max　=　document.getElementById("citysum");
+
     }
 
-    draw(cx, cy, r) {//描くメソッド
+    draw_point(cx, cy, r) {//描くメソッド
         this.ctx.beginPath();
         this.ctx.arc(cx, cy, r, 0, 2 * Math.PI, false);
         this.ctx.strokeStyle = 'rgb(0,0,0)';
         this.ctx.fill();
+        this.ctx.stroke();
+    }
+
+    draw_next() {//描くメソッド
+        this.ctx.beginPath();
+        var pi = this.start;
+        this.ctx.moveTo( pi.x, pi.y );
+        do {
+            this.ctx.lineTo( pi.x, pi. y );
+            pi = pi.next;
+        }while( pi != this.start )
+        this.ctx.lineTo( this.start.x, this.start.y );
         this.ctx.stroke();
     }
 
@@ -129,18 +187,21 @@ class Graphics {
 
     }
 
+    * challenge() {
+
+    }
     shortest(p) {//pからの最小を求める
         var short;
         var d_min = 999999999;//現状の最小
         for (let pi of this.pa) {//piに配列paを複製
-            if (pi != p) {
+            // if (pi != p) {
                 let d1 = p.distance(pi);//d1にp-pi間の距離を代入
                 if (d1 < d_min) {//新しいものが最小ならば
                     d_min = d1;//現状の最小を更新する
                     short = pi;//最小の終点を更新
                     console.log(pi);
                 }
-            }
+            // }
         }
 
         return short;//最短のものを返す
